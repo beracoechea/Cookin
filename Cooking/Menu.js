bab7327d2a73b870a6c2502b81c10a28670f980e
Francisco Beracoechea <jose.beracoechea5098@alumnos.udg.mx>
2024-04-23 13:42:32 -0600

    Se inicio a trabajar con el menu pero falta agregar todas las posibles ventanas

--- Cooking/Menu.js
+++ Cooking/Menu.js
@@ -1,12 +1,68 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Inicio from './Inicio';
+
+
+const Tab = createBottomTabNavigator();
 
 export default class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
+
   render() {
+    const { email } = this.props.route.params;
+
     return (
-      <View>
-        <Text>menu</Text>
-      </View>
-    )
+      <Tab.Navigator
+        tabBarOptions={null} // Eliminamos tabBarOptions
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Inicio') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'Chatbot') {
+              iconName = focused ? 'robot' : 'robot-outline';
+            } else if (route.name === 'CaminoAdiestramiento') {
+              iconName = focused ? 'tennis-ball' : 'tennis-ball';
+            } else if (route.name === 'Mapa') {
+              iconName = focused ? 'google-maps' : 'google-maps';
+            } else if (route.name === 'Cartilla') {
+              iconName = focused ? 'notebook-check' : 'notebook-check';
+            }
+
+            return <MaterialCommunityIcons name={iconName} color={color} size={size} />;
+          },
+          tabBarActiveTintColor: '#456B6B', 
+          tabBarInactiveTintColor: '#999999', 
+          tabBarStyle: {
+            backgroundColor: '#ffffff', 
+            borderTopWidth: 1, 
+            borderTopColor: '#cccccc', 
+            paddingTop: 5, 
+            paddingBottom: 5,
+          },
+        })}
+      >
+        <Tab.Screen
+          name="Inicio"
+          initialParams={{ email}}
+          options={{
+            tabBarLabel: 'Inicio',
+            headerShown: false,
+          }}
+        >
+          {(props) => <Inicio {...props} email={email} />}
+        </Tab.Screen>
+
+       
+      </Tab.Navigator>
+    );
   }
 }
