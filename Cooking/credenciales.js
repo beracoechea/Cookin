01bba29b114e6e1427e713365b89d01df4a088f4
aleck <sansepiol26@gmail.com>
2024-04-25 22:04:58 -0600

    Se resolvio el warning

--- Cooking/credenciales.js
+++ Cooking/credenciales.js
@@ -1,6 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 
 
@@ -15,10 +19,14 @@ const firebaseConfig = {
     appId: "1:798269608528:android:43bd4922f57e6490e5f755"
   };
 
-const appFirebase = initializeApp(firebaseConfig);
 
-export const FIREBASE_APP =initializeApp(firebaseConfig);
-export const FIREBASE_AUTH =getAuth(FIREBASE_APP);
-export const FIREBASE_DB =getFirestore(FIREBASE_APP);
+export const FIREBASE_APP = initializeApp(firebaseConfig);
 
+// initialize Firebase Auth for that app immediately
+const auth = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIREBASE_DB = getFirestore(FIREBASE_APP);
 
