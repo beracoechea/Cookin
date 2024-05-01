a44b80ab16a6ad658b5c9a1b8534bbf5885b425a
Francisco Beracoechea <jose.beracoechea5098@alumnos.udg.mx>
2024-05-01 14:16:59 -0600

    Agruegue imagenes

--- Cooking/imagenesRecetas.js
+++ Cooking/imagenesRecetas.js
@@ -5,6 +5,8 @@ const imagenesRecetas = {
   'Hotcakes.png':require('./Images/Recetas/Hotcakes.png'),
   'Enchiladassuizas.png':require('./Images/Recetas/Enchiladassuizas.png'),
   'ChuletasAlbañil.png':require('./Images/Recetas/ChuletasAlbañil.png'),
+  'CopazYogurt.png':require('./Images/Recetas/CopazYogurt.png'),
+  'YogurtAvena.png':require('./Images/Recetas/YogurtAvena.png'),
 };
 
 
